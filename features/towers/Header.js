3983fd436ac357884de45e5e7926afdfb70414b1
Delgado Lucas (Diseño - lanacion digital) <ldelgado@lanacion.com.ar>
2021-05-14 23:44:57 -0300

    fix phone

--- features/towers/Header.js
+++ features/towers/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import LocalPhoneIcon from '@material-ui/icons/LocalPhone';
 
 const Header = () => {
     return ( 
         <header className="header container">
             <a className="header__mail" href="mailto:lucas.matias.delgado@gmail.com">lucas.matias.delgado@gmail.com</a>
-            <a className="header__tel" href="tel:+591152610887"><LocalPhoneIcon/> 11-5261-0887</a>
+            <a className="header__tel" href="tel:+591152610887"> 11-5261-0887</a>
         </header> 
     );
 }
