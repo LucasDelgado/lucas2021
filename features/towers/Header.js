88c15b2f5ee2256c78404a4aa658465288a5adc2
Delgado Lucas (Diseño - lanacion digital) <ldelgado@lanacion.com.ar>
2021-05-18 10:40:16 -0300

    btn hidemode

--- features/towers/Header.js
+++ features/towers/Header.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const Header = () => {
+
+    const [darkMode, setDarkMode] = useState(false);
+
     return ( 
-        <header className="header container">
-            <a className="header__mail" href="mailto:lucas.matias.delgado@gmail.com">lucas.matias.delgado@gmail.com</a>
-            <a className="header__tel" href="tel:+591152610887"> 11-5261-0887</a>
+        <header className="header grid container">
+            <div className="header__left">
+                
+            </div>
+            <div className="header__right">
+                <button type="button" className="btn-primary --mb-16" onClick={ ()=>{ setDarkMode(!darkMode) } } >{ darkMode ? 'DarkMode' : 'LightMode' }</button>
+                <a className="header__mail" href="mailto:lucas.matias.delgado@gmail.com">lucas.matias.delgado@gmail.com</a>
+                <a className="header__tel" href="tel:+591152610887"> 11-5261-0887</a>
+            </div>
         </header> 
     );
 }
