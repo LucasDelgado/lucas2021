0800316a2647c6299d944686ec6de88d06114928
LucasDelgado <lucas.matias.delgado@gmail.com>
2023-08-19 22:23:23 -0300

    add new data

--- pages/index.js
+++ pages/index.js
@@ -13,44 +13,18 @@ const Home = (props) => {
           <span>{siteConfig.rol}</span>  
         </h1>
         <article className="home__descripPrincipal">
-          <p>Trabajo como desarrollador Web FrontEnd, actualmente en:
-            <strong> DIARIO LA NACION.</strong>
+          <p>I`m a Software Engineer in
+            <strong>Mercado Libre.</strong>
           </p>
           <p>
-            Con conocimientos solidos en: 
-            <strong>REACT.JS, HTML5, CSS3, JS, SASS, JAVA, SEO, UX, RESPONSIVE.</strong>
+            Main skills: 
+            <strong>REACT.JS, NODE.JS, GOLANG</strong>
           </p>
           <p>
-            Me encuentro cursando el 2do año de la carrera:
-            <strong>Analista de Sistemas en la ORT.</strong> 
+            Education:
+            <strong>Systems Analyst ORT</strong> 
           </p>
         </article>
-      </section>
-      <section className="skills">
-        <Title
-          title="Cursos certificados"/> 
-        <div className="grid grid-1-mobile">
-          <Card
-            title="DISEÑADOR UX"
-            descrip="Curso en donde implementamos el desarrollo UX de toda una app. Pasando por todos los procesos que esto conlleva."
-            place="en CODERHOUSE"/>
-
-          <Card
-            title="JS Vanilla"
-            descrip="En esta ocasión vimos lo nuevo de js y métodos para implementarlo y dejar de usar Jquery."
-            place="en EDUCACIONIT"/>
-
-          <Card
-            title="Desarrollo Web"
-            descrip="Vimos a fondo el desarrollo Responsive, jQuery, Bootstrap, Forms, y todo lo basico para comenzar a trabajar en la web."
-            place="en CODERHOUSE"/>
-        </div>
-      </section>
-
-      <section className="blog">
-        <Title
-          title="Blog"/> 
-          <p>Proximamente....</p>
       </section>
     </div>
   );
